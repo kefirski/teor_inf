b532e5f9eecb410e1c257260be5df678cbb1ef67
kefirski <olavursky@gmail.com>
2017-12-04 23:22:05 +0300

    Typo fix

--- model/model.py
+++ model/model.py
@@ -52,7 +52,7 @@ class Model(nn.Module):
 
         mask = t.eq(input.abs().sum(2), 0).data
 
-        out = self.out_attention(encoding, mask)
+        out = self.out_attention(input, mask)
         out = self.conv(out).squeeze(1)
         return self.fc(out)
 
