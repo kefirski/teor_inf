67b2029ce706c3242b841143af514a833c2938d5
kefirski <olavursky@gmail.com>
2017-12-04 15:50:08 +0300

    Fixes

--- model/utils/embedding.py
+++ model/utils/embedding.py
@@ -8,9 +8,6 @@ class Embedding(nn.Module):
     def __init__(self, path, vocab_size, embedding_size):
         super(Embedding, self).__init__()
 
-        self.max_len = max_len
-        self.embedding_size = embedding_size
-
         self.embeddings = nn.Embedding(vocab_size, embedding_size, padding_idx=0)
         self.embeddings.weight = nn.Parameter(t.from_numpy(np.load(path)).float(), requires_grad=False)
 
